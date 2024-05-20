21c917ae771c692c9b79eff6c7781f442ad1f10a
IT0198 <Konankidshanda@icsc.com.tw>
2024-05-20 11:01:59 +0800

    update filter when clicking hamburger

--- missions/task2/js/header.js
+++ missions/task2/js/header.js
@@ -21,5 +21,6 @@ $(document).ready(function() {
         $('.hamburger').toggle(50); // 切換菜單圖示顯示狀態
         $('.close').toggle(50); // 切換關閉圖示顯示狀態
         $('nav ul').slideToggle(300, 'swing');
+        $('.filter').toggle();
     });
 });
